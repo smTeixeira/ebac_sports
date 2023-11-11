db626991c8a3025f48a31383f6233e33bd977831
samuel de Matos Teixeira <samuelteixeira00@hotmail.com>
2023-11-11 18:51:24 -0300

    alterando o gerenciamento do estado do projeto para o Redux

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,17 +1,17 @@
+import { useSelector } from 'react-redux'
+import { RootReducer } from '../../store'
 import * as S from './styles'
 
-import { Produto } from '../../App'
-
-import cesta from '../../assets/cesta.png'
 import { paraReal } from '../Produto'
+import cesta from '../../assets/cesta.png'
 
-type Props = {
-  itensNoCarrinho: Produto[]
-  favoritos: Produto[]
-}
+const Header = () => {
+  const itens = useSelector((state: RootReducer) => state.carrinho.itens)
+  const itensFavoritos = useSelector(
+    (state: RootReducer) => state.favorito.itens
+  )
 
-const Header = ({ itensNoCarrinho, favoritos }: Props) => {
-  const valorTotal = itensNoCarrinho.reduce((acc, item) => {
+  const valorTotal = itens.reduce((acc, item) => {
     acc += item.preco
     return acc
   }, 0)
@@ -20,10 +20,10 @@ const Header = ({ itensNoCarrinho, favoritos }: Props) => {
     <S.Header>
       <h1>EBAC Sports</h1>
       <div>
-        <span>{favoritos.length} favoritos</span>
+        <span>{itensFavoritos.length} favoritos</span>
         <img src={cesta} />
         <span>
-          {itensNoCarrinho.length} itens, valor total: {paraReal(valorTotal)}
+          {itens.length} itens, valor total: {paraReal(valorTotal)}
         </span>
       </div>
     </S.Header>
